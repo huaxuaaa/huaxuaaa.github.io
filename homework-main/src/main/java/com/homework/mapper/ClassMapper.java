563dc765bdf6d295a0c2a8f9820eae9473e007a5
huaxuaaa <88487360+huaxuaaa@users.noreply.github.com>
2021-08-05 18:31:09 +0800

    Delete homework-main/src/main directory

--- homework-main/src/main/java/com/homework/mapper/ClassMapper.java
+++
@@ -1,7 +0,0 @@
-package com.homework.mapper;
-
-import com.homework.pojo.Class;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface ClassMapper extends Mapper<Class> {
-}
--- homework-main/src/main/java/com/homework/mapper/StudentMapper.java
+++
@@ -1,7 +0,0 @@
-package com.homework.mapper;
-
-import com.homework.pojo.Student;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface StudentMapper extends Mapper<Student> {
-}
